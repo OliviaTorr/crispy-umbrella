e214ef57b3068f99c7824e8c0025e8bf21605840
OliviaTorr <80424782+OliviaTorr@users.noreply.github.com>
2021-04-10 13:34:39 +0200

    Update Data.R

--- Data.R
+++ Data.R
@@ -1,91 +1,44 @@
 
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
 
-library(shiny)
-library(xfun)
 library(haven)
-library(randomizeR)
-library(blockTools)
-library(samplingbook)
+library(tidyverse)
 library(ggplot2)
-library(plotrix)
-library(survival)
-library(blockrand)
-library(tibble)
-library(ATE)
-library(vtable)
-library(tinytex)
-library(plm)
-library(lmtest)
+library(ivreg)
 
-```
-
-Problem Set 3 Analysis of Experimental Data: Project STAR
-
-1) Summary statistics
-```{r}
-data <- read_dta("STAR.dta")
-data$girl <- as.factor(data$girl)
-data$freelunk <- as.factor(data$freelunk)
-data$sck <- as.factor(data$sck)
-summary(data)
-aggregate(tscorek ~ sck, mean, data=data)
-max(data$totexpk_m)
-```
-There are 2795 girls and 2954 boys in the dataset.
-The mean outcome for all students (testscore) is 461.2 and the median is 457.5.
-The mean for the treated is 466 and for the control 459.1.
-The most experienced teacher has 324 months of experience, thats equal to 27 years of teaching.
+data <- read_dta("mortality.dta")
 
 
-2) OLS Regression
-```{r}
+## Simple OLS regressions with and without control variables 
 
-lm <- lm(data$tscorek ~ data$sck, data = data)
+lmwithout <- lm(data$before67dead ~ data$dist65_ageATend4emp, data = data)
+summary(lmwithout)
+
+lmwith <- lm(data$before67dead ~ data$dist65_ageATend4emp + data$nutsATage50 + data$halfyearOFbirth + data$czeit1yATage50 + data$czeit2yATage50 + data$czeit5yATage50 + data$czeit10yATage50 + data$czeit25yATage50, data = data)
+summary(lmwith)
+
+##Plotting densities
+
+plot1data <- data[which(data$Zd_during == 0), ]
+plot1 <- density(plot1data$dist65_ageATend4emp)
+plot(plot1)
+
+plot2data <- data[which(data$Zd_during == 1), ]
+plot2 <- density(plot2data$dist65_ageATend4emp)
+plot(plot2)
+
+###First stage regression
+
+lm <- lm(data$dist65_ageATend4emp ~ I(data$Zd_during*data$halfyearOFbirth) + data$nutsATage50 + data$halfyearOFbirth +
+           data$czeit1yATage50 + data$czeit2yATage50 + data$czeit5yATage50 + data$czeit10yATage50 + data$czeit25yATage50, data = data)
 summary(lm)
 
-plm <- plm(data$tscorek ~ data$sck, model = "within", index = c("schidkn"), data = data)
-summary(plm)
+##Second stage regression
 
-```
+IVwithcontrols <- ivreg(data$before67dead ~ data$dist65_ageATend4emp + data$nutsATage50 + data$halfyearOFbirth + data$czeit1yATage50 + data$czeit2yATage50 + data$czeit5yATage50 + data$czeit10yATage50 + data$czeit25yATage50| I(data$Zd_during*data$halfyearOFbirth) + data$nutsATage50 + data$halfyearOFbirth + data$czeit1yATage50 + data$czeit2yATage50 + data$czeit5yATage50 + data$czeit10yATage50 + data$czeit25yATage50, data = data)
+summary(IVwithcontrols)
 
-3) Standard Errors
-```{r}
-
-coeftest(plm, vcov. = vcovHC, type = "HC3")
-
-```
-
-4) Testing whether randomization worked (using Ws)
-```{r}
-
-data$sck <- as.numeric(data$sck)
-
-lmgirl <- lm(data$sck ~ data$girl, data = data)
-summary(lmgirl)
-
-lmfreelunk <- lm(data$sck ~ data$freelunk, data = data)
-summary(lmfreelunk)
-
-lmtotexp <- lm(data$sck ~ data$totexpk_m, data = data)
-summary(lmtotexp)
-
-totallm <- lm(data$sck ~ data$girl + data$freelunk + data$totexpk_m, data = data)
-anova(totallm)
-
-lmfrom2a <- lm(data$tscorek ~ data$sck + data$girl + data$freelunk + data$totexpk_m, data = data)
-summary(lmfrom2a)
-
-```
-
-5) Heterogeneity in Treatment Effects
-```{r}
-
-
-
-```
-
+IVwithoutcontrols <- ivreg(data$before67dead ~ data$dist65_ageATend4emp | I(data$Zd_during*data$halfyearOFbirth), data = data)
+summary(IVwithoutcontrols)
 
 
 
